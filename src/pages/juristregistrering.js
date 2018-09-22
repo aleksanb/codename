41e66d55ca80ec3784d95d75b95fc9101da4af0b
Mathilde-Kirkhus <mathilde.kirkhus@unacast.com>
2018-09-22 13:03:52 +0200

    use material-ui components

--- src/pages/juristregistrering.js
+++ src/pages/juristregistrering.js
@@ -1,21 +1,55 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
 import Layout from '../components/layout'
+import { Paper, Button, TextField, Grid, Avatar } from '@material-ui/core'
 
-const SecondPage = () => (
-    <Layout>
-        <h1>Meld deg inn som jurist  <FontAwesomeIcon icon="gavel"/></h1>
+const style = {
+    button: {
+        margin: '1em',
+    },
+    textfield: {
+        width: '80%',
+    }
+}
 
-        <p>Legg til LinkedIn profilen din her og opprett bruker: </p>
-        <form>
-            <label>
-                Url til LinkedIn profil:
-                <input type="text" name="linkedin-url" />
-            </label>
-            <input type="submit" value="Legg til" />
-        </form>
-    </Layout>
-)
+class JuristRegistreringPAge extends React.Component {
+    state = {
+        url: ""
+    };
 
-export default SecondPage
+    handleChange = name => event => {
+        this.setState({
+            [name]: event.target.value,
+        });
+    };
+
+    render() {
+        return (
+            <Layout>
+                <Grid container className="container" justify="center" spacing={24}>
+                    <Paper className="register-jurist-paper">
+                        <h1>Meld deg inn som jurist <i className="fas fa-gavel"></i></h1>
+
+                        <div>Legg til LinkedIn profilen din her og opprett bruker
+                        </div>
+                        <form>
+                            <TextField
+                                id="linkedin-url"
+                                label=" Url til profil:"
+                                value={this.state.url}
+                                onChange={this.handleChange('url')}
+                                margin="normal"
+                                style={style.textfield}
+                            />
+                        <div>
+                        <Button type="submit" variant="contained" color="primary" style={style.button}>
+                            Legg til
+                        </Button>
+                        </div>
+
+                        </form>
+                    </Paper>
+                </Grid>
+            </Layout>)
+}    }
+
+export default JuristRegistreringPAge
