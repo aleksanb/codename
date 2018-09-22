a1e3dabaaf3d788d87981cf4162ce1b661287ef2
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2018-09-22 11:47:47 +0200

    dajlkfasdlkjf

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql, Link } from 'gatsby'
 import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import './layout.css'
 
@@ -50,16 +51,9 @@ const Layout = ({ children }) => (
                   </Typography>
               </Toolbar>
           </AppBar>
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
+        <CssBaseline>
           {children}
-        </div>
+        </CssBaseline>
       </>
     )}
   />
