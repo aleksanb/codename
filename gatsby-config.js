1d4a6e0e6a4ccc04d5be0f56a539a277b2f4cfe9
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2018-09-22 10:03:36 +0200

    Boootstrap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Rettssamfunnet',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/components/layout.js
+++ src/components/layout.js
@@ -27,6 +27,11 @@ const Layout = ({ children }) => (
           ]}
         >
           <html lang="en" />
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"></link>
+
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous"></link>
+
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
