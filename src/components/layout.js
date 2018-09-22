ad771432f3168fd41804d5bfecc31ef48f6ca42a
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2018-09-22 11:45:20 +0200

    More stuff

--- src/components/layout.js
+++ src/components/layout.js
@@ -33,6 +33,7 @@ const Layout = ({ children }) => (
 
             <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"></link>
             <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
+            <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
        </Helmet>
           <AppBar position="static">
               <Toolbar>
