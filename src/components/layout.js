963836b90e9ed3e6c41196ae35965946ab2f49b8
Julie Johnsen Kirkhus <juliejk92@gmail.com>
2018-09-22 11:01:54 +0200

    front page stuff

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,6 +29,8 @@ const Layout = ({ children }) => (
           <html lang="en" />
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"></link>
 
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous"></link>
+
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous"></link>
 
           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,33 @@ import Layout from '../components/layout'
 const IndexPage = () => (
   <Layout>
     <div className="jumbotron">
-      <h2>Hallais, folkens</h2>
+      <div className="row search-box">
+        <div className="form-group">
+          <div className="col-12">
+            <input type="search" class="form-control" id="searchBox" placeholder="Fritt søk" />
+          </div>
+        </div>
+      </div>
+      <div className="row text-center">
+        <div className="col-xs-12 col-sm-6">
+          <i className="main-icons fas fa-gavel"></i>Arv
+        </div>
+        <div className="col-xs-12 col-sm-6">
+          <i className="main-icons fas fa-gavel"></i>Skatt
+        </div>
+        <div className="col-xs-12 col-sm-6">
+          <i className="main-icons fas fa-gavel"></i>Arv
+        </div>
+        <div className="col-xs-12 col-sm-6">
+          <i className="main-icons fas fa-gavel"></i>Skatt
+        </div>
+        <div className="col-xs-12 col-sm-6">
+          <i className="main-icons fas fa-gavel"></i>Arv
+        </div>
+        <div className="col-xs-12 col-sm-6">
+          <i className="main-icons fas fa-gavel"></i>ƒSkatt
+        </div>
+      </div>
     </div>
   </Layout>
 )
